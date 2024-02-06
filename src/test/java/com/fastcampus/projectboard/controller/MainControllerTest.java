2b88e41a9bea35510c330c579cf151ab0b0fb574
Heesu Kang <heesukang2000@gmail.com>
2024-02-07 00:25:17 +0900

    #54 - 문제가 된 테스트 코드 발견 및 수정
    이 부분은 진작에 개발 끝나고 테스트를 완료했던 코드인데,
    어느샌가 옛날 버전으로 돌아가 있다.
    정확한 원인은 알 수 없으나, 복구하고 테스트 완료.
    전체 테스트 통과함

--- src/test/java/com/fastcampus/projectboard/controller/MainControllerTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/MainControllerTest.java
@@ -1,9 +1,13 @@
 package com.fastcampus.projectboard.controller;
 
-import com.fastcampus.projectboard.config.SecurityConfig;
+import com.fastcampus.projectboard.config.TestSecurityConfig;
+import com.fastcampus.projectboard.service.ArticleService;
+import com.fastcampus.projectboard.service.PaginationService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
@@ -11,16 +15,20 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@Import(SecurityConfig.class)
+@DisplayName("View 루트 컨트롤러")
+@Import(TestSecurityConfig.class)
 @WebMvcTest(MainController.class)
 class MainControllerTest {
 
     private final MockMvc mvc;
 
+
+
     public MainControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
 
+    @DisplayName("[view][GET] 루트 페이지 -> 게시글 리스트 (레시판) 페이지 Redirection")
     @Test
     void givenNothing_whenRequestingRootPage_thenRedirectsToArticlesPage() throws Exception {
         // Given
@@ -29,8 +37,7 @@ class MainControllerTest {
         mvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("forward:/articles"))
-                .andExpect(forwardedUrl("/articles"))
-                .andDo(MockMvcResultHandlers.print());
+                .andExpect(forwardedUrl("/articles"));
     }
 
 }
